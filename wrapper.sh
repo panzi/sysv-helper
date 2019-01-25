281ed3fe763404a602f0c23cb7058c0036497994
Mathias Panzenböck <grosser.meister.morti@gmx.net>
2019-01-25 06:31:25 +0100

    more exit status handling fixes

--- wrapper.sh
+++ wrapper.sh
@@ -21,31 +21,31 @@ while [ : ]; do
 	set -e
 
 	echo
-	echo "Exit-Status: $STATUS"
 
 	mkdir -p "$SRVDIR"
 	echo "$STATUS" > "$SRVDIR/status"
 
+	# illegal PID?
+	if [[ $STATUS -eq 127 ]]; then
+		exit
+	fi
+
 	# WIFEXITED(STATUS)
 	if [[ $((STATUS & 0x7f)) -eq 0 ]]; then
-		STATUS=$(( (STATUS & 0xff00) >> 8 ))
-		echo "Exit-Status: $STATUS"
-		if [[ $RESTART != RESTART ]] || [[ $STATUS -eq 0 ]]; then
+		ACTUAL=$(( (STATUS & 0xff00) >> 8 ))
+		echo "Exit-Status: $ACTUAL"
+		if [[ $RESTART != RESTART ]] || [[ $ACTUAL -eq 0 ]]; then
 			exit
 		fi
-	fi
-	
 	# WIFSIGNALED(STATUS)
-	if [[ $(( ( (STATUS & 0x7f) + 1) >> 1 )) -gt 0 ]]; then
+	elif [[ $(( ( (STATUS & 0x7f) + 1) >> 1 )) -gt 0 ]]; then
 		SIGNAL=$(( STATUS & 0x7f ))
 		echo "Exit-Signal: $SIGNAL"
 		if [[ $RESTART != RESTART ]] || [[ $SIGNAL -eq 15 ]] || [[ $SIGNAL -eq 9 ]]; then
 			exit
 		fi
-	fi
-
-	if [[ $STATUS -eq 127 ]]; then
-		# illegal PID
+	else
+		echo "Unhandled-Exit-State: $STATUS"
 		exit
 	fi
 done
